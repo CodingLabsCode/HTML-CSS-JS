a7f95f84a8c5cef61ed58746a35be7c3a08601de
Coding Labs <baileyharbach14@outlook.com>
2026-06-07 11:35:55 +0100

    Refactor button variable names and add reset functionality

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -2,8 +2,13 @@ const city = document.querySelector("#city");
 const cityInput = document.querySelector("#cityInput")
 const temp = document.querySelector("#temp");
 const clouds = document.querySelector("#clouds");
-const btn = document.querySelector("#searchBtn");
+const searchBtn = document.querySelector("#searchBtn");
+const reset = document.querySelector("#reset")
 const apiKey = "API KEY HIDDEN"
+
+function error(msg){
+    return  `[ERROR] ${msg}`
+};
 
 async function getWeather() {
     const currentCity = cityInput.value
@@ -11,11 +16,23 @@ async function getWeather() {
     const response = await fetch (
         `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`
     )
+    if (response.status == 404) {
+        city.textContent = error("Invaild City")
+        setTimeout(() => {
+            city.textContent = ""
+        },2000)
+    } else {
         const data = await response.json()
         city.textContent = `City: ${data.name}`
         temp.textContent = `temp: ${data.main.temp}°C`
         clouds.textContent = `Cloudds: ${data.weather[0].description}`
+    }
     
 }
 
-btn.addEventListener("click", getWeather)
+searchBtn.addEventListener("click", getWeather)
+reset.addEventListener("click", () => {
+    city.textContent = ""
+    temp.textContent = ""
+    clouds.textContent = ""
+})
